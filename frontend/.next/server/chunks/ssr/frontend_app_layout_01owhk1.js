b089842197c033b362f3d45d14ab36642a9b3108
Rithvik-ak <rithvikkolipaka12@gmail.com>
2026-04-21 09:37:13 +0530

    Optimize frontend project structure for Vercel deployment

--- frontend/.next/server/chunks/ssr/frontend_app_layout_01owhk1.js
+++
@@ -1,3 +0,0 @@
-module.exports=[38558,a=>{"use strict";var b=a.i(7997);a.s(["default",0,function({children:a}){return(0,b.jsx)("html",{lang:"en",children:(0,b.jsx)("body",{style:{margin:0,padding:0,boxSizing:"border-box"},children:a})})},"metadata",0,{title:"Smart Expense Tracker",description:"AI-powered expense insights"}])},57313,a=>{a.n(a.i(38558))}];
-
-//# sourceMappingURL=frontend_app_layout_01owhk1.js.map
